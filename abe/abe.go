c52a79f961db513be47f2bbdbbc2b4786c5a618f
Tilen Marc <marct15@gmail.com>
2018-11-12 16:59:40 +0100

    implementing gaussian elemination

--- abe/abe.go
+++ abe/abe.go
@@ -5,6 +5,7 @@ import (
 	"github.com/fentec-project/gofe/sample"
 	"github.com/fentec-project/gofe/data"
 	"github.com/cloudflare/bn256"
+	"fmt"
 )
 
 type abeParams struct {
@@ -35,8 +36,9 @@ func (a Abe) GenerateMasterKeys() (*AbePubKey, data.Vector, error) {
 	if err != nil {
 		return nil, nil, err
 	}
-	t := sk.MulG2()
+	t := sk[:a.Params.l].MulG2()
 	y := bn256.Pair(new(bn256.G1), new(bn256.G2)) // is this the same as new(bn256.GT)?
+	y.ScalarMult(y, sk[a.Params.l])
 	return &AbePubKey{t: t, y: y,}, sk, nil
 }
 
@@ -61,6 +63,140 @@ func (a Abe) Encrypt(msg *bn256.GT, gamma []int, pk *AbePubKey) (*AbeCipher, err
 	return &AbeCipher{gamma: gamma, e0: e0, e: e,}, nil
 }
 
-func (a Abe) KeyGen(msp data.Matrix, msk data.Vector, mpk AbePubKey) (data.VectorG1) {
 
+type Msp struct {
+	mat data.Matrix
+	rows int
+	cols int
+	rowToI []int
+}
+
+func (a Abe) KeyGen(msp Msp, msk data.Vector, mpk AbePubKey) (data.VectorG1, error) {
+	u, err := getSum(msk[a.Params.l], a.Params.p, msp.cols)
+	if err != nil {
+		return nil, err
+	}
+	key := make(data.VectorG1, msp.rows)
+	for i := 0; i < msp.rows; i++ {
+		tMapIInv := new(big.Int).ModInverse(msk[msp.rowToI[i]], a.Params.p)
+		matTimesU, err := msp.mat[i].Dot(u)
+		if err != nil {
+			return nil, err
+		}
+		pow := new(big.Int).Mul(tMapIInv, matTimesU)
+		key[i] = new(bn256.G1).ScalarBaseMult(pow)
+	}
+
+	return key, nil
+}
+
+func getSum(y *big.Int, p *big.Int, d int) (data.Vector, error) {
+	ret := make(data.Vector, d)
+	sampler := sample.NewUniform(p)
+	var err error
+	sum := big.NewInt(0)
+	for i := 0; i < d - 1; i++ {
+		ret[i], err = sampler.Sample()
+		if err != nil {
+			return nil, err
+		}
+		sum.Add(sum, ret[i])
+		sum.Mod(sum, p)
+	}
+	ret[d-1] = new(big.Int).Sub(y, sum)
+	ret[d-1].Mod(ret[d-1], p)
+	return ret, nil
+}
+
+//func (a Abe) Decrypt(cipher *AbeCipher, key data.VectorG1, pk *AbePubKey) (bn256.GT, error) {
+//
+//}
+
+func gaussianElimination(mat data.Matrix, v data.Vector, p *big.Int) (data.Vector, error) {
+	cpMat := make([] data.Vector, len(mat))
+	u := make(data.Vector, len(mat))
+	fmt.Println(len(mat), len(mat[0]))
+	for i := 0; i < len(mat); i++ {
+		cpMat[i] = make(data.Vector, len(mat[0]))
+		for j := 0; j < len(mat[0]); j++ {
+			cpMat[i][j] = new(big.Int).Set(mat[i][j])
+		}
+		u[i] = new(big.Int).Set(v[i])
+	}
+	m, _ := data.NewMatrix(cpMat) // error is impossible to happen
+	ret := make(data.Vector, len(mat[0]))
+	h, k := 0, 0
+	for h < len(m) && k < len(m[0]) {
+		zero := true
+		for i := h; i < len(m); i++ {
+			if m[i][k].Sign() != 0 {
+				fmt.Println(h, i)
+				//fmt.Println(m)
+				m[h], m[i] = m[i], m[h]
+
+				u[h], u[i] = u[i], u[h]
+				zero = false
+				break
+			}
+		}
+		if zero {
+			if u[k].Sign() != 0 {
+				return nil, fmt.Errorf("no solution")
+			} else {
+				ret[k] = big.NewInt(0)
+			}
+			k++
+			continue
+		}
+		mHKInv := new(big.Int).ModInverse(m[h][k], p)
+		fmt.Println(mHKInv, "inv")
+		for i := h + 1; i < len(m); i++ {
+			f := new(big.Int).Mul(mHKInv, m[i][k])
+			fmt.Println(f, "f")
+			m[i][k] = big.NewInt(0)
+			for j := k + 1; j < len(m[0]); j++ {
+				fmt.Println(m[i][j], m[h][j], new(big.Int).Mul(f, m[h][j]))
+				m[i][j].Sub(m[i][j], new(big.Int).Mul(f, m[h][j]))
+				fmt.Println(m[i][j])
+				m[i][j].Mod(m[i][j], p)
+			}
+			u[i].Sub(u[i], new(big.Int).Mul(f, u[h]))
+			u[i].Mod(u[i], p)
+		}
+		fmt.Println(m)
+		k++
+		h++
+	}
+	for i := h; i < len(m); i++ {
+		if u[i].Sign() != 0 {
+			return nil, fmt.Errorf("no solution")
+		}
+	}
+	for j := k; j < len(m[0]); j++ {
+		ret[j] = big.NewInt(0)
+	}
+
+	fmt.Println(m, u)
+	//fmt.Println(h, k)
+	fmt.Println(ret)
+	for h > 0 {
+		h--
+		for k > 0 {
+			k--
+			//fmt.Println(ret[k])
+			if ret[k] == nil {
+				//fmt.Println(ret[k], 2)
+				//fmt.Println(len(m[h][k+1:]), len(ret[k+1:]))
+				tmpSum, _ := m[h][k+1:].Dot(ret[k+1:])
+				ret[k] = new(big.Int).Sub(u[h], tmpSum)
+				mHKInv := new(big.Int).ModInverse(m[h][k], p)
+				ret[k].Mul(ret[k], mHKInv)
+				ret[k].Mod(ret[k], p)
+				break
+			}
+		}
+	}
+	//fmt.Println(ret)
+
+	return ret, nil
 }
--- abe/abe_test.go
+++ abe/abe_test.go
@@ -3,9 +3,29 @@ package abe
 import (
 	"testing"
 	"fmt"
+	"math/big"
+	"github.com/fentec-project/gofe/sample"
+	"github.com/fentec-project/gofe/data"
 )
 
 func TestAbe(t *testing.T) {
 	a := newAbe(10)
 	fmt.Println(a)
+}
+
+func TestGaussianElimintaion(t *testing.T) {
+	p := big.NewInt(5)
+	sampler := sample.NewUniform(p)
+	mat, err := data.NewRandomMatrix(3, 3, sampler)
+	if err != nil {
+		t.Fatalf("Error during matrix generation: %v", err)
+	}
+	v, err := data.NewRandomVector(3, sampler)
+	fmt.Println(mat, v)
+	x, err := gaussianElimination(mat, v, p)
+	fmt.Println(x)
+	vCheck, err := mat.MulVec(x)
+	vCheck = vCheck.Mod(p)
+	//assert.Equal(t, v, vCheck)
+	fmt.Println(v, vCheck)
 }
