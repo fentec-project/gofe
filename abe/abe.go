746004c7eaf129c30945538d3a751d4220e602a4
Tilen Marc <marct15@gmail.com>
2018-11-19 16:49:48 +0100

    working on policy transformation

--- abe/abe.go
+++ abe/abe.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"math/big"
 
+	"strconv"
+	"strings"
+
 	"github.com/cloudflare/bn256"
 	"github.com/fentec-project/gofe/data"
 	"github.com/fentec-project/gofe/sample"
@@ -76,7 +79,7 @@ type Msp struct {
 	rowToAttrib []int
 }
 
-func (a Abe) KeyGen(msp Msp, sk data.Vector) (data.VectorG1, error) {
+func (a Abe) KeyGen(msp *Msp, sk data.Vector) (data.VectorG1, error) {
 	if len(msp.mat) == 0 || len(msp.mat[0]) == 0 {
 		return nil, fmt.Errorf("empty msp matrix")
 	}
@@ -124,7 +127,7 @@ type AbeKey struct {
 	rowToAttrib []int
 }
 
-func (a Abe) DelagateKeys(keys data.VectorG1, msp Msp, attrib []int) *AbeKey {
+func (a Abe) DelagateKeys(keys data.VectorG1, msp *Msp, attrib []int) *AbeKey {
 	attribMap := make(map[int]bool)
 	for _, e := range attrib {
 		attribMap[e] = true
@@ -153,6 +156,9 @@ func (a Abe) Decrypt(cipher *AbeCipher, key *AbeKey) (*bn256.GT, error) {
 	}
 
 	alpha, err := gaussianElimination(key.mat.Transpose(), ones, a.Params.p)
+	ww, _ := key.mat.Transpose().MulVec(alpha)
+	ww = ww.Mod(a.Params.p)
+	fmt.Println("here2", ww)
 	if err != nil {
 		return nil, err
 	}
@@ -167,11 +173,169 @@ func (a Abe) Decrypt(cipher *AbeCipher, key *AbeKey) (*bn256.GT, error) {
 	return ret, nil
 }
 
+// BooleanToMsp takes as an input a boolean expression (without a NOT gate) and
+// outputs a msp structure representing the expression, i.e. a matrix whose rows
+// correspond to attributes used in the expression and with the property that a
+// boolean expression assigning 1 to some attributes is satisfied iff the
+// corresponding rows span a vector [1, 1,..., 1] in Z_p, and a vector whose i-th entry
+// indicates to which attribute the i-th row corresponds.
+func BooleanToMsp(boolExp string, p *big.Int) (*Msp, error) {
+	vec := make(data.Vector, 1)
+	vec[0] = big.NewInt(1)
+	msp, _, err := booleanToMspIterative(boolExp, vec, 1)
+	if err != nil {
+		return nil, err
+	}
+
+	// create an invertible matrix that maps [1, 0,..., 0] to [1,1,...,1]
+	perm := make(data.Matrix, len(msp.mat[0]))
+	fmt.Println(msp.mat)
+	for i:=0; i < len(msp.mat[0]); i++ {
+		perm[i] = make(data.Vector, len(msp.mat[0]))
+		for j:=0; j < len(msp.mat[0]); j++ {
+			if i == 0 || j == i {
+				perm[i][j] = big.NewInt(1)
+			} else {
+				perm[i][j] = big.NewInt(0)
+			}
+
+		}
+	}
+	fmt.Println(perm)
+	msp.mat, err = msp.mat.Mul(perm)
+	msp.mat = msp.mat.Mod(p)
+	return msp, err
+}
+
+// booleanToMspIterative iteratively builds a msp structure by splitting the expression
+// into two parts separated by an AND or OR gate, generating a msp structure on each of
+// them, and joining both structures together. The structure is such the boolean expression
+// assigning 1 to some attributes is satisfied iff the corresponding rows span a vector
+// [1, 0,..., 0]. The algorithm is known as Lewko-Waters algorithm, see Appendix G in
+// https://eprint.iacr.org/2010/351.pdf.
+func booleanToMspIterative(boolExp string, vec data.Vector, c int) (*Msp, int, error) {
+	boolExp = strings.TrimSpace(boolExp)
+	numBrc := 0
+	var boolExp1 string
+	var boolExp2 string
+	var c1 int
+	var cOut int
+	var msp1 *Msp
+	var msp2 *Msp
+	var err error
+	found := false
+
+	// find the main AND or OR gate
+	for i, e := range boolExp {
+		if e == '(' {
+			numBrc++
+			continue
+		}
+		if e == ')' {
+			numBrc--
+			continue
+		}
+		if numBrc == 0 && i < len(boolExp)-3 && boolExp[i:i+3] == "AND" {
+			boolExp1 = boolExp[:i]
+			boolExp2 = boolExp[i+3:]
+			vec1, vec2 := makeAndVecs(vec, c)
+			msp1, c1, err = booleanToMspIterative(boolExp1, vec1, c+1)
+			if err != nil {
+				return nil, 0, err
+			}
+			msp2, cOut, err = booleanToMspIterative(boolExp2, vec2, c1)
+			if err != nil {
+				return nil, 0, err
+			}
+			found = true
+			break
+		}
+		if numBrc == 0 && i < len(boolExp)-2 && boolExp[i:i+2] == "OR" {
+			boolExp1 = boolExp[:i]
+			boolExp2 = boolExp[i+2:]
+			msp1, c1, err = booleanToMspIterative(boolExp1, vec, c)
+			if err != nil {
+				return nil, 0, err
+			}
+			msp2, cOut, err = booleanToMspIterative(boolExp2, vec, c1)
+			if err != nil {
+				return nil, 0, err
+			}
+			found = true
+			break
+		}
+	}
+
+	// If the AND or OR gate is not found then there are two options,
+	// ether the whole expression is in brackets, or the the expression
+	// is only one attribute. It nether of both is is true, then
+	// an error is returned while converting the expression into an
+	// attribute
+	if found == false {
+		if boolExp[0] == '(' && boolExp[len(boolExp)-1] == ')' {
+			boolExp = boolExp[1:(len(boolExp) - 1)]
+			return booleanToMspIterative(boolExp, vec, c)
+		}
+
+		attrib, err := strconv.Atoi(boolExp)
+		if err != nil {
+			return nil, 0, err
+		}
+		mat := make(data.Matrix, 1)
+		mat[0] = make(data.Vector, c)
+		for i := 0; i < c; i++ {
+			mat[0][i] = new(big.Int).Set(vec[i])
+		}
+		rowToAttrib := make([]int, 1)
+		rowToAttrib[0] = attrib
+		return &Msp{mat: mat, rowToAttrib: rowToAttrib}, c, nil
+	} else {
+		// otherwise we join the two msp structures into one
+		mat := make(data.Matrix, len(msp1.mat)+len(msp2.mat))
+		for i := 0; i < len(msp1.mat); i++ {
+			mat[i] = make(data.Vector, cOut)
+			for j := 0; j < len(msp1.mat[0]); j++ {
+				mat[i][j] = msp1.mat[i][j]
+			}
+			for j := len(msp1.mat[0]); j < cOut; j++ {
+				mat[i][j] = big.NewInt(0)
+			}
+		}
+		for i := 0; i < len(msp2.mat); i++ {
+			mat[i+len(msp1.mat)] = msp2.mat[i]
+		}
+		rowToAttrib := append(msp1.rowToAttrib, msp2.rowToAttrib...)
+
+		return &Msp{mat: mat, rowToAttrib: rowToAttrib}, cOut, nil
+	}
+}
+
+// makeAndVecs is a helping structure that given a vector and and counter
+// creates two new vectors used whenever an AND gate is found in a iterative
+// step of BooleanToMsp
+func makeAndVecs(vec data.Vector, c int) (data.Vector, data.Vector) {
+	vec1 := make(data.Vector, c+1)
+	vec2 := make(data.Vector, c+1)
+	for i := 0; i < c; i++ {
+		vec1[i] = big.NewInt(0)
+		if i < len(vec) {
+			vec2[i] = new(big.Int).Set(vec[i])
+		} else {
+			vec2[i] = big.NewInt(0)
+		}
+	}
+	vec1[c] = big.NewInt(-1)
+	vec2[c] = big.NewInt(1)
+
+	return vec1, vec2
+}
+
 // gaussianElimination solves a vector equation mat * x = v and finds vector x,
 // using Gaussian elimination. Arithmetic operations are considered to be over
 // Z_p, where p should be a prime number. If such x does not exist, then the
 // function returns an error.
 func gaussianElimination(mat data.Matrix, v data.Vector, p *big.Int) (data.Vector, error) {
+	fmt.Println(mat, v, p)
 	if len(mat) == 0 || len(mat[0]) == 0 {
 		return nil, fmt.Errorf("the matrix should not be empty")
 	}
@@ -206,11 +370,7 @@ func gaussianElimination(mat data.Matrix, v data.Vector, p *big.Int) (data.Vecto
 			}
 		}
 		if zero {
-			if u[k].Sign() != 0 {
-				return nil, fmt.Errorf("no solution")
-			} else {
-				ret[k] = big.NewInt(0)
-			}
+			ret[k] = big.NewInt(0)
 			k++
 			continue
 		}
